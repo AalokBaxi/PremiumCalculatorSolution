932d80d1c53671494fc5d6992bc6e461e75d920e
aalok <a@test.com>
2021-04-15 23:51:30 +1000

    UT file draft

---
+++ PremiumCalculatorTest/UnitTestCollection.cs
@@ -0,0 +1,25 @@
+using PremiumCalculator.Service;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using PremiumCalculator.Data.Models;
+using PremiumCalculator.Service.Common;
+using System;
+
+namespace PremiumCalculator.Test
+{
+    [TestClass]
+    public class UnitTestCollection
+    {
+        [TestMethod]
+        public void CalculateValidation_Fail()
+        {
+            
+        }
+
+        [TestMethod]
+        public void CalculateValidation()
+        {
+            
+        }
+    }
+}
