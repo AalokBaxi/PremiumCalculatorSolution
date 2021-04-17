be88cda1b0c35ca622c9a53a13b62ee6d6594cf2
aalok <a@test.com>
2021-04-18 03:58:56 +1000

    Swagger changes for deployment to Azure

--- PremiumCalculatorApi/Startup.cs
+++ PremiumCalculatorApi/Startup.cs
@@ -64,10 +64,11 @@ namespace PremiumCalculator.Api
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PremiumCalculatorApi v1"));
+                app.UseDeveloperExceptionPage();                
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PremiumCalculatorApi v1"));
 
             app.UseCors(o => o.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
