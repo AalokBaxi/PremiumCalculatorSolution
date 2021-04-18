1f1eb463073fee016f715d65e769addb35a5f607
aalok <a@test.com>
2021-04-18 19:24:56 +1000

    Created unit test methods

--- PremiumCalculatorTest/UnitTestCollection.cs
+++ PremiumCalculatorTest/UnitTestCollection.cs
@@ -1,20 +1,36 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NUnit.Framework;
+using PremiumCalculator.Core.Models;
+using PremiumCalculator.Service.Services;
+using System;
+using PremiumCalculator.Core.Common;
 
 namespace PremiumCalculator.Test
 {
-    [TestClass]
     public class UnitTestCollection
-    {
-        [TestMethod]
-        public void CalculateValidation_Fail()
+    {        
+        [Test]        
+        public void CalculateValidation_Throw_When_ParameterIsNull()
         {
-            
+            var _occupationService = new Mock<IOccupationService>();            
+            var _calculatorService = new CalculatorService(_occupationService.Object);
+
+            var ex = Assert.Throws<Exception>(() => _calculatorService.CalculateMonthlyPremium(null));
+            Assert.AreSame(Constants.CALCULATOR_PARAMETER_NULL_ERROR, ex.Message);            
         }
 
-        [TestMethod]
-        public void CalculateValidation()
-        {
-            
+        [Test]
+        public void CalculateValidation_Validate_Result()
+        {                        
+            var _occupationService = new Mock<IOccupationService>();
+            var _calculatorService = new CalculatorService(_occupationService.Object);
+
+            _occupationService.Setup(x => x.GetOccupationByID(1)).Returns(
+                new Occupation { ID = 1, Name = "Cleaner", RatingDetail = new RatingDetail { ID = 3, Name = "Light Manual", Factor = (decimal)1.50 } });
+
+            var actualResults =_calculatorService.CalculateMonthlyPremium(new CalculatorParameter { DeathSumInsured = 10000, OccupationID = 1, Age = 38 });
+
+            Assert.AreEqual(6840m, actualResults);
         }
     }
 }
